4404efca3da0121410586c6ee19780708d26e397
NafisOfficial <nafisiqbal.net2002@gmail.com>
2023-06-12 17:08:05 +0600

    set user booked api done

--- index.js
+++ index.js
@@ -65,6 +65,12 @@ async function run() {
       res.send(result);
     })
 
+    app.get('/userBooked',async(req,res)=>{
+      const cursor = bookedClassesCollection.find();
+      const result = await cursor.toArray()
+      res.send(result);
+  })
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
